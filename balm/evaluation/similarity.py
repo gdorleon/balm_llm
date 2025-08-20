ee6d90e10827e4c427c14edbc3431fa59c89fed0
G. Dorleon <19830770+gdorleon@users.noreply.github.com>
2025-08-20 21:01:41 +0700

    Update similarity.py

--- balm/evaluation/similarity.py
+++ balm/evaluation/similarity.py
@@ -2,6 +2,6 @@ from typing import List
 import sacrebleu
 
 def self_bleu_similarity(a: str, b: str) -> float:
-    # Convert BLEU to a similarity in [0,1]
+    # Here we compute BLEU score between two sentences and normalize to [0,1]
     bleu = sacrebleu.sentence_bleu(a, [b]).score / 100.0
     return bleu
